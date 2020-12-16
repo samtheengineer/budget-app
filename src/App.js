8339dc601647cc908cbeb67834a9aa92be00da4b
Samuel Wood <samuel.wood@macys.com>
2020-12-16 13:48:38 -0500

    Using React components DoughnutChart and LineChart to clean code in App.js

--- src/App.js
+++ src/App.js
@@ -1,88 +1,28 @@
 import React from 'react';
 import './App.css';
 import './bootstrap.min.css'
-import { Line, Doughnut } from 'react-chartjs-2';
+import DoughnutChart from './components/DoughnutChart';
+import LineChart from './components/LineChart'
 
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'Katie',
-        fill: false,
-        lineTension: 0.1,
-        borderColor: 'rgba(205, 162, 252, 1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        // borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(205, 162, 252, 1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
-      },
-      {
-        label: 'Sam',
-        fill: false,
-        lineTension: 0.1,
-        borderColor: 'rgba(255, 146, 104, 1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        // borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(255, 146, 104, 1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [100, 40, 30, 81, 45, 55, 72]
-      }
-    ]
-  };
-  const costData = {
-    labels: [
-      'Rent',
-      'Utilities',
-      'Groceries',
-      'Gas',
-    ],
-    datasets: [
-      {
-        backgroundColor:['rgba(105, 183, 255, 1)', 'rgba(105, 255, 63, 1)', 'rgba(255, 146, 104, 1)', 'rgba(255, 52, 63, 1)'],
-        data: [1440, 400, 300, 60]
-      }
-    ]
-  }
 function App() {
   return (
     <div className="container-fluid">
       <h1 className="text-center">Budget App</h1>
       <div className="row">
-        <div id="lineChart">
-          <Line useRef="chart" data={data} />
-        </div>
+        <LineChart />
       </div>
       <div className="row">
-        <div className="col-md-6">
-          <h2 className="text-center">Necessary Costs</h2>
-          <div id="reqcostDoughnut">
-            <Doughnut useRef="chart" data={costData} />
-          </div>
+      <div className="col-md-4">
+          <h2 className="text-center">All Costs</h2>
+          <DoughnutChart />
         </div>
-        <div className="col-md-6">
-        <h2 className="text-center">Extra Costs</h2>
-          <div id="funcostDoughnut">
-            <Doughnut useRef="chart" data={costData} />
-          </div>
+        <div className="col-md-4">
+          <h2 className="text-center">Necessary Costs</h2>
+          <DoughnutChart />
+        </div>
+        <div className="col-md-4">
+          <h2 className="text-center">Extra Costs</h2>
+          <DoughnutChart />
         </div>
       </div>
       
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -4,33 +4,52 @@ import { Line } from 'react-chartjs-2';
 const data = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
-    {
-        label: 'My First dataset',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
-    }]
+        {
+            label: 'Katie',
+            fill: false,
+            lineTension: 0.1,
+            borderColor: 'rgba(129,137,110)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            // borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(205, 162, 252, 1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: 'Sam',
+            fill: false,
+            lineTension: 0.1,
+            borderColor: 'rgba	(102,122,116)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            // borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(255, 146, 104, 1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [100, 40, 30, 81, 45, 55, 72]
+        }
+    ]
 };
 function LineChart() {
     return (
-        <div>
-            <h2>Line Example</h2>
-            <Line ref="chart" data={data} />
+        <div id="lineChart">
+            <Line useRef="chart" data={data} />
         </div>
     )
 }
