b2afc8222799a137c2befb6af248fae953f8cf32
Samuel Wood <samuel.wood@macys.com>
2020-12-16 11:12:38 -0500

    initial commit. Most content is in App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import './bootstrap.min.css'
+import { Line, Doughnut } from 'react-chartjs-2';
 
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Katie',
+        fill: false,
+        lineTension: 0.1,
+        borderColor: 'rgba(205, 162, 252, 1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        // borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(205, 162, 252, 1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: 'Sam',
+        fill: false,
+        lineTension: 0.1,
+        borderColor: 'rgba(255, 146, 104, 1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        // borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(255, 146, 104, 1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [100, 40, 30, 81, 45, 55, 72]
+      }
+    ]
+  };
+  const costData = {
+    labels: [
+      'Rent',
+      'Utilities',
+      'Groceries',
+      'Gas',
+    ],
+    datasets: [
+      {
+        backgroundColor:['rgba(105, 183, 255, 1)', 'rgba(105, 255, 63, 1)', 'rgba(255, 146, 104, 1)', 'rgba(255, 52, 63, 1)'],
+        data: [1440, 400, 300, 60]
+      }
+    ]
+  }
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="container-fluid">
+      <h1 className="text-center">Budget App</h1>
+      <div className="row">
+        <div id="lineChart">
+          <Line useRef="chart" data={data} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          <h2 className="text-center">Necessary Costs</h2>
+          <div id="reqcostDoughnut">
+            <Doughnut useRef="chart" data={costData} />
+          </div>
+        </div>
+        <div className="col-md-6">
+        <h2 className="text-center">Extra Costs</h2>
+          <div id="funcostDoughnut">
+            <Doughnut useRef="chart" data={costData} />
+          </div>
+        </div>
+      </div>
+      
+      
     </div>
   );
 }
